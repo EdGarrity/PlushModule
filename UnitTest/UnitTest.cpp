e89c1c16c13ab01f70c765ff7e7969416e986bfc
Ed Garrity <30885192+EdGarrity@users.noreply.github.com>
2025-03-14 18:33:10 -0400

    Refactor unit tests for Plush environment
    Comment out PlushUnitTest class and remove StackManipulation's TestMethod1.
    Add Push_One_Integer test to BooleanInstructions to validate stack state after pushing an integer (609679783) instead of using BOOLEAN.= instruction.
    Update assertions to reflect the new expected stack state.

--- UnitTest/UnitTest.cpp
+++ UnitTest/UnitTest.cpp
@@ -9,30 +9,43 @@ Plush::Environment env;
 
 namespace PlushUnitTest
 {
-	TEST_CLASS(PlushUnitTest)
-	{
-	public:
-		
-		TEST_METHOD(TestMethod1)
-		{
-		}
-	};
+	//TEST_CLASS(PlushUnitTest)
+	//{
+	//public:
+	//	
+	//	TEST_METHOD(TestMethod1)
+	//	{
+	//	}
+	//};
 
-	TEST_CLASS(BooleanInstructions)
+	//TEST_CLASS(BooleanInstructions)
+	//{
+	//public:
+	//	TEST_METHOD(EQUALS_No_Parameters)
+	//	{
+	//		Assert::IsTrue(is_stack_state(env, {}, {}, {}, {}, {}));
+
+	//		env.enable_function("BOOLEAN.=");
+
+	//		processor.run(env, "{:instruction BOOLEAN.= :close 0}");
+
+	//		Assert::IsTrue(is_stack_state(env, {}, {}, {}, {},
+	//			{
+	//				CodeAtom("{:instruction BOOLEAN.= :close 0}")
+	//			}));
+	//	}
+	//};
+
+	TEST_CLASS(StackManipulation)
 	{
 	public:
-		TEST_METHOD(EQUALS_No_Parameters)
+		TEST_METHOD(Push_One_Integer)
 		{
 			Assert::IsTrue(is_stack_state(env, {}, {}, {}, {}, {}));
 
-			env.enable_function("BOOLEAN.=");
+			processor.run(env, "{:instruction 609679783 :close  0}");
 
-			processor.run(env, "{:instruction BOOLEAN.= :close 0}");
-
-			Assert::IsTrue(is_stack_state(env, {}, {}, {}, {},
-				{
-					CodeAtom("{:instruction BOOLEAN.= :close 0}")
-				}));
+			Assert::IsTrue(is_stack_state(env, { 609679783 }, {}, {}, {}, {}));
 		}
 	};
 }
