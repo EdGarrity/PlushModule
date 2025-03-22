3b32bb92cbd4e4f90652f017e861f5d12d1dce2c
Ed Garrity <30885192+EdGarrity@users.noreply.github.com>
2025-03-21 23:06:06 -0400

    Add Utilities/String module and refactor string functions
    Updated `PlushModule.vcxproj` to include `Utilities/String.cpp` and `Utilities/String.h`. Modified project filters accordingly. Refactored `String.cpp` by removing several string manipulation functions and improving `isFloat` and `isNumber` implementations. Adjusted `String.h` to reflect these changes and enhance performance by changing `isNumber` to accept a constant reference.

--- PlushModule/Utilities/String.cpp
+++ PlushModule/Utilities/String.cpp
@@ -1,125 +1,24 @@
 #include "String.h"
-
-#include <string>
-#include <sstream>
-#include <vector>
-#include <iterator>
-#include <algorithm>
-#include <iostream>
+#include <stdexcept>
 
 namespace Utilities
 {
-	// https://stackoverflow.com/questions/236129/how-do-i-iterate-over-the-words-of-a-string
-	template<typename Out>
-	void split(const std::string &s, char delim, Out result) {
-		std::stringstream ss(s);
-		std::string item;
-		while (std::getline(ss, item, delim)) {
-			*(result++) = item;
+	bool isNumber(const std::string& str) 
+	{
+		try 
+		{
+			std::size_t pos;
+			long num = std::stol(str, &pos);	// Store the return value to avoid C6031 and C4834
+			(void)num;							// Explicitly ignore the variable to avoid unused variable warning
+			return pos == str.size();
 		}
-	}
-
-	std::vector<std::string> split(const std::string &s, char delim)
-	{
-		std::vector<std::string> elems;
-		split(s, delim, std::back_inserter(elems));
-		return elems;
-	}
-
-	// convert string to upper case
-	std::string to_upper(std::string &data)
-	{
-		std::for_each(data.begin(), data.end(), [](char & c)
+		catch (const std::invalid_argument&) 
 		{
-			c = ::toupper(c);
-		});
-
-		return data;
-	}
-
-	// convert string to lower case
-	std::string to_lower(std::string &data)
-	{
-		std::for_each(data.begin(), data.end(), [](char & c)
-		{
-			c = ::tolower(c);
-		});
-
-		return data;
-	}
-
-	// convert copy of string to upper case
-	std::string to_upper_copy(std::string data)
-	{
-		std::string newData = data;
-
-		return to_upper(newData);
-	}
-
-	// convert copy of string to lower case
-	std::string to_lower_copy(std::string data)
-	{
-		std::string newData = data;
-
-		return to_lower(newData);
-	}
-
-	bool isFloat(std::string str)
-	{
-		//std::istringstream iss(myString);
-		//float f;
-		//iss >> std::noskipws >> f; // noskipws considers leading whitespace invalid
-		//// Check the entire string was consumed and if either failbit or badbit is set
-		//return iss.eof() && !iss.fail();
-
-		// See https://en.cppreference.com/w/cpp/string/basic_string/stol
-		std::size_t pos{};
-		try
-		{
-			const double i{ std::stod(str, &pos) };
-		}
-		catch (std::invalid_argument const& /*ex*/)
-		{
-			//std::cerr << "isFloat(" << str << ") std::invalid_argument::what() : " << ex.what() << '\n';
 			return false;
 		}
-		catch (std::out_of_range const& /*ex*/)
+		catch (const std::out_of_range&) 
 		{
-			//std::cerr << "isFloat(" << str << ") std::out_of_range::what(): " << ex.what() << '\n';
 			return false;
 		}
-
-		return true;
-	}
-
-	bool isNumber(const std::string str)
-	{
-		//for (char const& c : str) 
-		//{
-		//	if (std::isdigit(c) == 0) return false;
-		//}
-		//return true;
-
-		// See https://en.cppreference.com/w/cpp/string/basic_string/stol
-		std::size_t pos{};
-		try
-		{
-			const long i{ std::stol(str, &pos) };
-	
-			if (str[pos] == '.')
-				return false;
-		}
-		catch (std::invalid_argument const& /*ex*/)
-		{
-			//std::cerr << "isNumber(" << str << ") std::invalid_argument::what() : " << ex.what() << '\n';
-			return false;
-		}
-		catch (std::out_of_range const& /*ex*/)
-		{
-			//std::cerr << "isNumber(" << str << ") std::out_of_range::what(): " << ex.what() << '\n';
-			return false;
-		}
-
-		return true;
 	}
 }
